d12bbaffbe5460165f33c4189ceadf12f9e20e8f
franqq <githae.francis@gmail.com>
2016-01-09 13:00:23 +0300

    Modified logic to get admitted patient information

--- omod/src/main/java/org/openmrs/module/ipdui/page/controller/PatientInfoPageController.java
+++ omod/src/main/java/org/openmrs/module/ipdui/page/controller/PatientInfoPageController.java
@@ -1,21 +1,28 @@
 package org.openmrs.module.ipdui.page.controller;
 
+import org.openmrs.Patient;
+import org.openmrs.api.PatientService;
 import org.openmrs.api.context.Context;
 import org.openmrs.module.hospitalcore.IpdService;
 import org.openmrs.module.hospitalcore.model.IpdPatientAdmitted;
-import org.openmrs.ui.framework.Model;
+import org.openmrs.ui.framework.page.PageModel;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 /**
  * Created by Francis on 1/7/2016.
  */
 public class PatientInfoPageController {
-    public void get(@RequestParam(required = false) int searchPatientID, Model model) {
+    public void get(@RequestParam(value = "search",required = true) String search, PageModel model) {
 
         IpdService ipdService = (IpdService) Context.getService(IpdService.class);
+        PatientService patientService = Context.getService(PatientService.class);
 
-        IpdPatientAdmitted patientInformation = ipdService.getAdmittedByPatientId(searchPatientID);
-        model.addAttribute("patientInformation", patientInformation);
+        List<Patient> patientList = patientService.getPatients(null,search, null, true,null,null);
+        IpdPatientAdmitted patientInformation = ipdService.getAdmittedByPatientId(patientList.get(0).getPatientId());
+        model.addAttribute("patientInformation",patientInformation );
 
     }
+
 }
